238d503864785807afadee099e44f8051d22dbfa
Chocochip101 <fingercut3822@gmail.com>
2023-02-23 18:38:52 +0900

    Init: First commit

---
+++ src/main/java/com/chocochip/amaji/AmajiApplication.java
@@ -0,0 +1,13 @@
+package com.chocochip.amaji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmajiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmajiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chocochip/amaji/AmajiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chocochip.amaji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmajiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
