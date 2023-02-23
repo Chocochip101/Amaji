971abc5c5ab8311bfbf0818d72b247eb9dc54f93
Chocochip101 <fingercut3822@gmail.com>
2023-02-23 20:07:58 +0900

    Feat: Member Entity 추가 name, nickname, email, role 필드

--- src/main/java/com/chocochip/amaji/member/domain/Member.java
+++ src/main/java/com/chocochip/amaji/member/domain/Member.java
@@ -1,4 +1,41 @@
 package com.chocochip.amaji.member.domain;
 
+import com.chocochip.amaji.global.util.Role;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String member_name;
+    @Column(nullable = false)
+    private String member_nickname;
+
+    private String member_email;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role member_role;
+
+    public boolean hasNickname() {
+        return !Objects.isNull(member_nickname);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return Objects.equals(id, member.id);
+    }
 }
