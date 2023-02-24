e275f8fbc74dcb52a7f8f56aa8ab58e22e95d0f0
Chocochip101 <fingercut3822@gmail.com>
2023-02-24 21:58:39 +0900

    Feat: Optional<Member> FindByEmailAndProvider() 구현

--- src/main/java/com/chocochip/amaji/member/domain/repository/MemberRepository.java
+++ src/main/java/com/chocochip/amaji/member/domain/repository/MemberRepository.java
@@ -3,5 +3,7 @@ package com.chocochip.amaji.member.domain.repository;
 import com.chocochip.amaji.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
 }
--- src/main/java/com/chocochip/amaji/member/domain/repository/MemberRepositoryImpl.java
+++ src/main/java/com/chocochip/amaji/member/domain/repository/MemberRepositoryImpl.java
@@ -1,7 +1,28 @@
 package com.chocochip.amaji.member.domain.repository;
 
+import com.chocochip.amaji.member.domain.Member;
+import com.chocochip.amaji.security.custom.OAuthProvider;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.Optional;
+
+import static com.chocochip.amaji.member.domain.QMember.member;
 
 @Repository
 public class MemberRepositoryImpl implements MemberRepositoryCustom {
+    private final JPAQueryFactory queryFactory;
+
+    public MemberRepositoryImpl(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    public Optional<Member> findByEmailAndProvider(String email, OAuthProvider provider){
+        return queryFactory
+                .selectFrom(member)
+                .where(member.email.eq(email), member.oauth.provider.eq(provider))
+                .stream().findAny();
+    }
+
 }
