c9b0f65c8dd69f70967bb2330faa6b00ed4d1a86
Chocochip101 <fingercut3822@gmail.com>
2023-02-24 22:01:28 +0900

    Feat: MemberService TODO

--- src/main/java/com/chocochip/amaji/member/service/MemberService.java
+++ src/main/java/com/chocochip/amaji/member/service/MemberService.java
@@ -1,4 +1,19 @@
 package com.chocochip.amaji.member.service;
 
+import com.chocochip.amaji.member.domain.repository.MemberRepository;
+import com.chocochip.amaji.member.ui.dto.MemberLoginResponse;
+import com.chocochip.amaji.member.ui.dto.MemberLoginResquest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
 public class MemberService {
+//    private final MemberRepository memberRepository;
+
+
 }
