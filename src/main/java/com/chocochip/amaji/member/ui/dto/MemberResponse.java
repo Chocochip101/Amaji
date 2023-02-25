dd66bf0298bd8a5019c575b1fa778592ed41bf67
Chocochip101 <fingercut3822@gmail.com>
2023-02-25 16:25:14 +0900

    Feat: Member 조회용 response dto 추가

--- src/main/java/com/chocochip/amaji/member/ui/dto/MemberResponse.java
+++ src/main/java/com/chocochip/amaji/member/ui/dto/MemberResponse.java
@@ -1,4 +1,17 @@
 package com.chocochip.amaji.member.ui.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class MemberResponse {
+    private Long user_id;
+
+    private String user_nickname;
+
+    private String user_picture_url;
+
 }
