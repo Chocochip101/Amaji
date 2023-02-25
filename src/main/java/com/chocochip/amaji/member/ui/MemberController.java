1b680b5c507d78eda1438f8a0e82c199ce0851f4
Chocochip101 <fingercut3822@gmail.com>
2023-02-25 16:25:43 +0900

    Feat: Member Controller 추가 - changeNickname, getOneMember

--- src/main/java/com/chocochip/amaji/member/ui/MemberController.java
+++ src/main/java/com/chocochip/amaji/member/ui/MemberController.java
@@ -1,4 +1,33 @@
 package com.chocochip.amaji.member.ui;
 
+import com.chocochip.amaji.member.service.MemberService;
+import com.chocochip.amaji.member.ui.dto.MemberNicknameRequest;
+import com.chocochip.amaji.member.ui.dto.MemberResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
 public class MemberController {
+
+    private final MemberService memberService;
+    @PatchMapping("/{user-id}/nickname")
+    public ResponseEntity<Void> changeNickname(
+            @PathVariable("user-id") Long memberId,
+            @RequestBody @Valid MemberNicknameRequest memberNicknameRequest){
+        memberService.changeMemberNickname(memberId, memberNicknameRequest);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+    }
+
+    @GetMapping("/{user-id}")
+    public ResponseEntity<MemberResponse> getOneMember(
+            @PathVariable("user-id") Long memberId){
+        MemberResponse memberResponse = memberService.findOneMember(memberId);
+        return ResponseEntity.status(HttpStatus.OK).body(memberResponse);
+    }
 }
