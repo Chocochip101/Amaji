5b23c08cad15272226b892cb636de1e987e1b938
Chocochip101 <fingercut3822@gmail.com>
2023-02-25 17:35:52 +0900

    Test: city entity @Builder 패턴 Test

---
+++ src/test/java/com/chocochip/amaji/city/domain/CityTest.java
@@ -0,0 +1,23 @@
+package com.chocochip.amaji.city.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class CityTest {
+    @DisplayName("도시를 빌더 패턴을 활용하여 생성할 수 있다")
+    @Test
+    public void createCity() throws Exception {
+        //given
+        String cityName = "Osaka";
+
+        //when
+        City city = City.builder()
+                .name(cityName).build();
+
+        //then
+        Assertions.assertEquals(city.getName(), cityName);
+    }
+
+}
