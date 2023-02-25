9e847aac305a64cf9f5ed9cebb4076ede74b7005
Chocochip101 <fingercut3822@gmail.com>
2023-02-25 16:26:49 +0900

    Feat: Member Service changeMemberNickname, findOneMember 구현

--- src/main/java/com/chocochip/amaji/member/service/MemberService.java
+++ src/main/java/com/chocochip/amaji/member/service/MemberService.java
@@ -1,19 +1,32 @@
 package com.chocochip.amaji.member.service;
 
+import com.chocochip.amaji.member.domain.Member;
 import com.chocochip.amaji.member.domain.repository.MemberRepository;
-import com.chocochip.amaji.member.ui.dto.MemberLoginResponse;
-import com.chocochip.amaji.member.ui.dto.MemberLoginResquest;
+import com.chocochip.amaji.member.mapper.MemberMapper;
+import com.chocochip.amaji.member.ui.dto.MemberNicknameRequest;
+import com.chocochip.amaji.member.ui.dto.MemberResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Slf4j
 public class MemberService {
-//    private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
+    @Transactional
+    public void changeMemberNickname(Long memberId, MemberNicknameRequest memberNicknameRequest){
+        memberRepository.updateMemberNickname(memberId, memberNicknameRequest.getNickname());
+    }
+
+    public MemberResponse findOneMember(Long memberId){
+        Optional<Member> optionalMember = memberRepository.findOneMemberByMemberId(memberId);
+        return MemberMapper.INSTANCE.memberEntityToResponse(optionalMember.get());
+    }
 
 }
