a06857d178e9e6f30b1bad8e9b1f45ecde1ad9e1
Chocochip101 <fingercut3822@gmail.com>
2023-02-25 18:32:05 +0900

    Feat: 모든 도시 조회 함수 추가

---
+++ src/main/java/com/chocochip/amaji/city/service/CityService.java
@@ -0,0 +1,28 @@
+package com.chocochip.amaji.city.service;
+
+import com.chocochip.amaji.city.domain.City;
+import com.chocochip.amaji.city.domain.CityEnum;
+import com.chocochip.amaji.city.domain.repository.CityRepository;
+import com.chocochip.amaji.city.mapper.CityMapper;
+import com.chocochip.amaji.city.ui.dto.CityResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
+public class CityService {
+
+    private final CityRepository cityRepository;
+
+    public List<CityResponse> findAllCities(){
+        List<City> cityList = cityRepository.findAll();
+        return cityList.stream().map(c -> CityMapper.INSTANCE.cityEntityToResponse(c)).collect(Collectors.toList());
+    }
+}
