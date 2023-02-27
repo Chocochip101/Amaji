ec6dca37c601787ebf46acd85ad37cc7910bd003
Chocochip101 <fingercut3822@gmail.com>
2023-02-27 19:47:13 +0900

    Feat: Restaurant findByMember, findByCity 추가

--- src/main/java/com/chocochip/amaji/restaurant/domain/repository/RestaurantRepositoryCustom.java
+++ src/main/java/com/chocochip/amaji/restaurant/domain/repository/RestaurantRepositoryCustom.java
@@ -1,5 +1,12 @@
 package com.chocochip.amaji.restaurant.domain.repository;
 
 import com.chocochip.amaji.global.util.CityEnum;
+import com.chocochip.amaji.restaurant.domain.Restaurant;
+
+import java.util.List;
+
 public interface RestaurantRepositoryCustom {
+    List<Restaurant> findRestaurantListByMemberId(Long memberId);
+
+    List<Restaurant> findRestaurantByCityName(CityEnum cityName);
 }
