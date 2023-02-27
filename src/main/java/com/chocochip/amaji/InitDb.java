baf76804d6b49644b56c07f65a0ce06d281510b3
Chocochip101 <fingercut3822@gmail.com>
2023-02-27 20:49:04 +0900

    Feat: initDb 리뷰 추가

--- src/main/java/com/chocochip/amaji/InitDb.java
+++ src/main/java/com/chocochip/amaji/InitDb.java
@@ -8,6 +8,8 @@ import com.chocochip.amaji.member.domain.Member;
 import com.chocochip.amaji.member.domain.repository.MemberRepository;
 import com.chocochip.amaji.restaurant.domain.Restaurant;
 import com.chocochip.amaji.restaurant.domain.repository.RestaurantRepository;
+import com.chocochip.amaji.review.domain.Review;
+import com.chocochip.amaji.review.domain.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -33,9 +35,11 @@ public class InitDb {
         private final MemberRepository memberRepository;
         private final RestaurantRepository restaurantRepository;
 
+        private final ReviewRepository reviewRepository;
+
         private final CityRepository cityRepository;
         public void dbInit1() {
-            Member member1 = getBuild(1L, "권기호", "dev.chocochip@gmail.com", "chocochip", "aaa", Role.USER);
+            Member member1 = getMember(1L, "권기호", "dev.chocochip@gmail.com", "chocochip", "aaa", Role.USER);
             memberRepository.save(member1);
 
             City cityOsaka = getCity(CityEnum.OSAKA);
@@ -49,6 +53,19 @@ public class InitDb {
 
             Restaurant restaurant2 = getRestaurant(cityTokyo, 2L, "토리카츠 치킨 시부야", "일본 〒150-0043 Tokyo, Shibuya City, Dogenzaka, 2 Chome−16−19 都路ビル 2F", 4.0, 139.69753829454956,35.66100801163414);
             restaurantRepository.save(restaurant2);
+
+            Review review1 = getReview(restaurant1, "https://m.blog.naver.com/bryanlikes/221558048613");
+            reviewRepository.save(review1);
+
+            Review review2 = getReview(restaurant1, "https://m.blog.naver.com/choco0429/221472148603");
+            reviewRepository.save(review2);
+
+            Review review3 = getReview(restaurant1, "https://lightcircle.tistory.com/41");
+            reviewRepository.save(review3);
+        }
+
+        private static Review getReview(Restaurant restaurant, String url) {
+            return  Review.builder().restaurant(restaurant).url(url).build();
         }
 
         private static City getCity(CityEnum cityName) {
@@ -69,7 +86,7 @@ public class InitDb {
             return restaurant;
         }
 
-        private static Member getBuild(Long id, String name, String email, String nickname, String pic, Role role) {
+        private static Member getMember(Long id, String name, String email, String nickname, String pic, Role role) {
             return Member.builder()
                     .id(id)
                     .name(name)
