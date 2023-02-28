20c2dad6e8d91474bf76ab16b8cd901bdcc0a0f3
Chocochip101 <fingercut3822@gmail.com>
2023-02-28 16:11:26 +0900

    Feat: Menu 응답용 dto

--- src/main/java/com/chocochip/amaji/menu/ui/dto/MenuResponse.java
+++ src/main/java/com/chocochip/amaji/menu/ui/dto/MenuResponse.java
@@ -1,13 +1,20 @@
 package com.chocochip.amaji.menu.ui.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class MenuResponse {
-    private String menu_picture_url;
+    private String menu_name;
+    private Integer menu_price;
+    private List<String> menu_picture_url;
+
 }
