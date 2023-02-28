97a853bd936fb8eb9ceab4a10593351192b6af9b
Chocochip101 <fingercut3822@gmail.com>
2023-02-28 14:13:18 +0900

    Feat: MenuRestaurant와 Menu 연관관계 설정

--- src/main/java/com/chocochip/amaji/menuResturant/domain/MenuRestaurant.java
+++ src/main/java/com/chocochip/amaji/menuResturant/domain/MenuRestaurant.java
@@ -1,5 +1,6 @@
 package com.chocochip.amaji.menuResturant.domain;
 
+import com.chocochip.amaji.menu.domain.Menu;
 import com.chocochip.amaji.restaurant.domain.Restaurant;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,9 +20,9 @@ public class MenuRestaurant {
     @JoinColumn(name = "restaurant_id")
     private Restaurant restaurant;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "menu_id")
-//    private Menu menu;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_id")
+    private Menu menu;
 
     @Enumerated(EnumType.STRING)
     private MenuSort menu_sort = MenuSort.ETC; //메뉴의 종류
