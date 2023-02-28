d7da53834ef0e17ada80a563754f02ab407d1113
Chocochip101 <fingercut3822@gmail.com>
2023-02-28 16:11:52 +0900

    Feat: Menu 조회용 API by restaurantId

--- src/main/java/com/chocochip/amaji/menu/ui/MenuController.java
+++ src/main/java/com/chocochip/amaji/menu/ui/MenuController.java
@@ -1,4 +1,28 @@
 package com.chocochip.amaji.menu.ui;
 
+import com.chocochip.amaji.menu.domain.repository.MenuRepository;
+import com.chocochip.amaji.menu.service.MenuService;
+import com.chocochip.amaji.menu.ui.dto.MenuResponse;
+import com.chocochip.amaji.restaurant.ui.dto.RestaurantDetailResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/menu")
 public class MenuController {
+    private final MenuService menuService;
+
+    @GetMapping("/restaurant/{restaurant-id}")
+    public ResponseEntity<List<MenuResponse>> getAllMenuByRestaurantId(@PathVariable("restaurant-id") Long restaurantId){
+        List<MenuResponse> menuResponseList = menuService.findAllMenuByRestaurant(restaurantId);
+        return ResponseEntity.status(HttpStatus.OK).body(menuResponseList);
+    }
 }
