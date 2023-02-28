11da065fa6fa0550607ab9782a8e435a08db1350
Chocochip101 <fingercut3822@gmail.com>
2023-02-28 14:29:53 +0900

    Feat: MenuResponse dto 추가

--- src/main/java/com/chocochip/amaji/menu/ui/dto/MenuResponse.java
+++ src/main/java/com/chocochip/amaji/menu/ui/dto/MenuResponse.java
@@ -1,4 +1,13 @@
 package com.chocochip.amaji.menu.ui.dto;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class MenuResponse {
+    private String menu_picture_url;
 }
