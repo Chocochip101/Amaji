14febcef174883c907ce4ab23faae5bea5d16e96
Chocochip101 <fingercut3822@gmail.com>
2023-02-28 14:21:01 +0900

    Feat: MenuPicture entity 추가

--- src/main/java/com/chocochip/amaji/menu/domain/MenuPicture.java
+++ src/main/java/com/chocochip/amaji/menu/domain/MenuPicture.java
@@ -1,4 +1,25 @@
 package com.chocochip.amaji.menu.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class MenuPicture {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "menu_picture_id")
+    private Long id;
+
+    @Column(name = "menu_picture_url")
+    private String url;
+
+
+    //=========  다대일   =========//
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_id")
+    private Menu menu;
 }
