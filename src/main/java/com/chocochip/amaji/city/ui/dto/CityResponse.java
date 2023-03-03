cb20009d343a6ca65bbfdb74bd128ec6c97e9292
Chocochip101 <fingercut3822@gmail.com>
2023-03-03 20:55:31 +0900

    Feat: Response dto에 @AllArgsConstructor 추가

--- src/main/java/com/chocochip/amaji/city/ui/dto/CityResponse.java
+++ src/main/java/com/chocochip/amaji/city/ui/dto/CityResponse.java
@@ -2,10 +2,12 @@ package com.chocochip.amaji.city.ui.dto;
 
 
 import com.chocochip.amaji.global.util.CityEnum;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
