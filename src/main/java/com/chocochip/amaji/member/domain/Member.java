416c27a31bc44b859e8e3be66cd9685a6b5323f3
Chocochip101 <fingercut3822@gmail.com>
2023-03-03 21:19:08 +0900

    Refactor: Member와 Oauth Entity 합체

--- src/main/java/com/chocochip/amaji/member/domain/Member.java
+++ src/main/java/com/chocochip/amaji/member/domain/Member.java
@@ -1,10 +1,9 @@
 package com.chocochip.amaji.member.domain;
 
+import com.chocochip.amaji.global.entity.BaseEntity;
 import com.chocochip.amaji.global.util.Role;
 import com.chocochip.amaji.memberRestaurant.domain.MemberRestaurant;
-import com.chocochip.amaji.menuResturant.domain.MenuRestaurant;
-import com.chocochip.amaji.oauth.domain.Oauth;
-import com.chocochip.amaji.restaurant.domain.Restaurant;
+import com.chocochip.amaji.oauth.domain.AuthProvider;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,7 +16,7 @@ import java.util.Objects;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Member {
+public class Member extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
@@ -39,19 +38,22 @@ public class Member {
     @Enumerated(EnumType.STRING)
     private Role member_role = Role.GUEST;
 
+    private String refreshToken;
+
+    @Enumerated(EnumType.STRING)
+    private AuthProvider authProvider;
+
+
     @Column(nullable = false)
     private boolean isDeleted = Boolean.FALSE;
 
-    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "oauth_id")
-    private Oauth oauth;
 
     //=========  일대다   =========//
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<MemberRestaurant> memberRestaurantList = new ArrayList<>();
 
     @Builder
-    public Member(Long id, String name, String nickname, String email, String picture, Role member_role, boolean isDeleted, Oauth oauth) {
+    public Member(Long id, String name, String nickname, String email, String picture, Role member_role, boolean isDeleted, String refreshToken, AuthProvider authProvider) {
         this.id = id;
         this.name = name;
         this.nickname = nickname;
@@ -59,7 +61,8 @@ public class Member {
         this.picture = picture;
         this.member_role = member_role;
         this.isDeleted = isDeleted;
-        this.oauth = oauth;
+        this.refreshToken = refreshToken;
+        this.authProvider = authProvider;
     }
 
 
@@ -78,11 +81,6 @@ public class Member {
     public String getRoleKey() {
         return this.member_role.getKey();
     }
-
-    public Member update(String name, String email) {
-        this.name = name;
-        this.email = email;
-        return this;
-    }
+    
 
 }
