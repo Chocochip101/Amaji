7626d86ea67d4f90c797eb4c23770d6062f65e56
Chocochip101 <fingercut3822@gmail.com>
2023-03-03 21:35:02 +0900

    Feat: Token refresh

---
+++ src/main/java/com/chocochip/amaji/oauth/service/AuthService.java
@@ -0,0 +1,57 @@
+package com.chocochip.amaji.oauth.service;
+
+import com.chocochip.amaji.member.domain.repository.MemberRepository;
+import com.chocochip.amaji.oauth.util.CookieUtil;
+import com.chocochip.amaji.security.custom.CustomUserDetails;
+import com.chocochip.amaji.security.custom.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Cookie;
+
+@Log4j2
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class AuthService {
+    @Value("${jwt.refresh-cookie-key}")
+    private String cookieKey;
+
+    private final MemberRepository memberRepository;
+    private final JwtTokenProvider tokenProvider;
+
+    public String refreshToken(HttpServletRequest request, HttpServletResponse response, String oldAccessToken) {
+        // 1. Validation Refresh Token
+        String oldRefreshToken = CookieUtil.getCookie(request, cookieKey)
+                .map(Cookie::getValue).orElseThrow(() -> new RuntimeException("no Refresh Token Cookie"));
+
+        if (!tokenProvider.validateToken(oldRefreshToken)) {
+            throw new RuntimeException("Not Validated Refresh Token");
+        }
+
+        // 2. 유저정보 얻기
+        Authentication authentication = tokenProvider.getAuthentication(oldAccessToken);
+        CustomUserDetails user = (CustomUserDetails) authentication.getPrincipal();
+
+        Long id = Long.valueOf(user.getName());
+
+        // 3. Match Refresh Token
+        String savedToken = memberRepository.getRefreshTokenById(id);
+
+        if (!savedToken.equals(oldRefreshToken)) {
+            throw new RuntimeException("Not Matched Refresh Token");
+        }
+
+        // 4. JWT 갱신
+        String accessToken = tokenProvider.createAccessToken(authentication);
+        tokenProvider.createRefreshToken(authentication, response);
+
+        return accessToken;
+    }
+}
