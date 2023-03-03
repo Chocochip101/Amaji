5b4c513f064f27b59818d935ec7d6f69feb4d0c8
Chocochip101 <fingercut3822@gmail.com>
2023-03-03 21:20:04 +0900

    Feat: Member Repository findByEmail 추가

--- src/main/java/com/chocochip/amaji/member/domain/repository/MemberRepository.java
+++ src/main/java/com/chocochip/amaji/member/domain/repository/MemberRepository.java
@@ -1,9 +1,13 @@
 package com.chocochip.amaji.member.domain.repository;
 
 import com.chocochip.amaji.member.domain.Member;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+    Optional<Member> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
